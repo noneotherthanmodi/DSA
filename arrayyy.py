485ab9d009f538297aa699b53c7dd703f1e45de6
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-11 23:31:45 +0530

    arrays

--- arrayyy.py
+++ arrayyy.py
@@ -1,18 +1,20 @@
 #MAX SIZE OF AN ARRAY THAT CAN BE DEFINED INSIDE AN INT MAIN FUNC IS 10^6, AND GLOBALLY 10^7
 
-# def larg_ele(arr:[int],n:int):
-#     largest = arr[0]
-#     for i in range(n):
-#         if (arr[i]>largest):
-#             largest = arr[i]
-#     print(largest)
-        
-
-
 def larg_ele(arr:[int],n:int):
-    arr.sort()
-    print(arr)
-    print(arr[-1])
+    largest = arr[0]
+    for i in range(n):
+        if (arr[i]>largest):
+            largest = arr[i]
+    print(largest)
+        
         
 larg_ele([7,2,1,5,2],5) 
 
+#SECOND LARGEST ELEMENT:
+
+def second_largest(arr:[int],n:int):
+    largest = arr[0]
+    for i in range(n):
+        if arr[i]>largest:
+            largest = arr[i]
+        
