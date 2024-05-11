ba05d1297ef96df0b67ff37585aa6164a73e5cdc
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-12 02:20:34 +0530

    arrays

--- arrayyy.py
+++ arrayyy.py
@@ -27,3 +27,34 @@ def second_largest_element(arr:[int],n:int):
 
 second_largest_element([7,2,1,5,2],5) 
 second_largest_element([642 ,642 ,642, 642, 642, 642 ,642 ,642, 642, 642, 642, 642, 642],13) 
+
+
+from sys import maxsize
+def second_smallest(arr:[int],n:int):
+    smallest = arr[0]
+    for i in range(n):
+        if(arr[i]<=smallest):
+            smallest = arr[i]
+
+    second_smallest = maxsize
+    for i in range(n):
+        if(arr[i]<second_smallest and arr[i]!=smallest):
+            second_smallest = arr[i]
+
+    print(second_smallest)
+
+second_smallest([7,2,1,3,5,2],6)
+
+
+#REMOVE DUPLICATE ELEMENTS FROM AN ARRAY:
+def remove_duplicate(arr:[int],n:int):
+    arr.sort()
+    sorted_arr = []
+    for i in range(0,n):
+        if(arr[i]!=arr[i+1]):
+            sorted_arr.append(arr[i])
+
+    print(sorted_arr)
+
+
+remove_duplicate([7,2,1,5,2],5) 
