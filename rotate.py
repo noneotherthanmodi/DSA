df152fac963530d23ea0cf54bfdd93413cb054df
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-13 04:43:22 +0530

    arrays rotation

--- rotate.py
+++ rotate.py
@@ -4,7 +4,7 @@ def rotate(arr:[int],k:int):
     temp = arr[n-1]
     
     
-    for j in range(1,n):
+    for j in range(n-1,0,-1):
         arr[j] = arr[j-1]
         
     arr[0] = temp
@@ -15,14 +15,13 @@ rotate([1,2,3,4,5,6,7],3)
 
 
 #ROTATE ARRAY TO LEFT 
-def rotate_left(arr:[int],k:int):
+def rotate_left(arr:[int]):
     temp = arr[0]
     n = len(arr)
-    for j in range(k):
-        for i in range(1,n):
-            arr[i-1] = arr[i]
-        
-        arr[n-1] = temp
-        print(arr)
+    for i in range(1,n):
+        arr[i-1] = arr[i]
+    
+    arr[n-1] = temp
+    print(arr)
 
-rotate_left([1,2,3,4,5,6,7],3)
+rotate_left([1,2,3,4,5,6,7])
