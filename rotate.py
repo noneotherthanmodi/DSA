7d5500d26fe729a7500ec16beaa85eba39112e3b
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-13 05:29:26 +0530

    arrays rotation

--- rotate.py
+++ rotate.py
@@ -1,27 +1,51 @@
 #ROTATE ARRAY TO THE RIGHT BY K STEPS
-def rotate(arr:[int],k:int):
+def rotate_right(arr:[int],k:int):
     n = len(arr)
-    temp = arr[n-1]
+    k = k % n
+    if k ==0:
+        return
+    temp = []
+    for i in range(n-k,n):
+        temp.append(arr[i])
+
+    # temp = arr[n-1]
     
     
-    for j in range(n-1,0,-1):
-        arr[j] = arr[j-1]
+    for j in range(n-1,k-1,-1):
+        arr[j] = arr[j-k]
         
-    arr[0] = temp
+    for i in range(0,k):
+        arr[i] = temp[i]
+    # arr[0] = temp
 
     print(arr)
-
-rotate([1,2,3,4,5,6,7],3)
+rotate_right([-1],1)
+rotate_right([1,2,3,4,5,6,7],3)
 
 
 #ROTATE ARRAY TO LEFT 
-def rotate_left(arr:[int]):
-    temp = arr[0]
+def rotate_left(arr:[int],k:int):
+    temp = []
     n = len(arr)
-    for i in range(1,n):
-        arr[i-1] = arr[i]
+    k = k % n
+    if k == 0:
+        return
+
+
+    for i in range(k):
+        temp.append(arr[i])
     
-    arr[n-1] = temp
+    
+    for i in range(k,n):
+        arr[i-k] = arr[i]
+    
+
+    for j in range(n-k,n):
+        arr[j] = temp[j-(n-k)]
+
+    
     print(arr)
 
-rotate_left([1,2,3,4,5,6,7])
+
+rotate_left([-1],1)
+rotate_left([1,2,3,4,5,6,7],3)
