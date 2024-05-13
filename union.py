fe012f359cf23b3ca87dbf0c56b3146a4b35b4aa
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-14 01:52:07 +0530

    union of arrays

---
+++ union.py
@@ -0,0 +1,16 @@
+def union_of_array(arr1,arr2,m,n):
+    st = set()
+    union = []
+    for num in arr1:
+        st.add(num)
+
+    for num in arr2:
+        st.add(num)
+
+    for s in st:
+        union.append(s)
+
+    union.sort()
+
+    print(union)
+union_of_array([1,2,3,4,6,7,7,6],[3,4,5,4,1,2,3,9],8,8) 
