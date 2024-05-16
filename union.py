1451ac5d7effda6b3c8c841f8f82441904c53625
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-16 18:10:04 +0530

    union of arrays

--- union.py
+++ union.py
@@ -1,4 +1,6 @@
-def union_of_array(arr1,arr2,m,n):
+def union_of_array(arr1,arr2):
+    m = len(arr1)
+    n = len(arr2)
     st = set()
     union = []
     for num in arr1:
@@ -13,8 +15,6 @@ def union_of_array(arr1,arr2,m,n):
     union.sort()
 
     print(union)
-union_of_array([1,2,3,4,6,7,7,6],[3,4,5,4,1,2,3,9],8,8) 
-
 
 #USING TWO POINTER 
 def union_using_two_pointer(arr1,arr2):
@@ -49,39 +49,11 @@ def union_using_two_pointer(arr1,arr2):
 
     print(union_array)
 
-union_using_two_pointer([1,2,3,4,6,7,7,6],[3,4,5,4,1,2,3,9]) 
 
 
-def sortedArray(a: [int], b: [int]) -> [int]:
-    # Write your code here
-    
-    n = len(a)
-    m = len(b)
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr2 = [1, 2, 3, 4, 4, 5, 11, 12]
 
-    unionArray = []
-    i = 0
-    j = 0
 
-    while(i<n and j<m):
-        if (a[i]<=b[j]):
-            if (len(unionArray) ==0 or unionArray[-1] != a[i]):
-                unionArray.append(a[i])
-            i += 1
-        else:    
-            if(len(unionArray) == 0 or unionArray[-1] != b[j]):
-                unionArray.append(b[j])
-            j += 1
-
-    while(j<m):
-        if(len(unionArray) == 0 or unionArray[-1] != b[j]):
-            unionArray.append(b[j])
-        j+=1
-    while(i<n):
-        if(len(unionArray) == 0 or unionArray[-1] != a[i]):
-            unionArray.append(a[i])
-        i += 1
-
-    print(unionArray)
-    return unionArray
-
-sortedArray([1,2,3,4,6,7,7,6],[3,4,5,4,1,2,3,9]) 
+union_of_array(arr1,arr2)
+union_using_two_pointer(arr1,arr2)
