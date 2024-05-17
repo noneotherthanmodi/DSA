55634a3f1a57fe2e7d1564a940faaa5ddebfdd87
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-17 16:28:54 +0530

    finding missing values in an array

--- find_num.py
+++ find_num.py
@@ -1,19 +1,22 @@
 from typing import List
 def missingNumber(a : List[int], N : int) -> int:
     # Write your code here.
-    flag = 0
-    for i in range(1,N):
-        for j in range(0,N-1):
+    
+    for i in range(1,N+1):
+        flag = 0
+        for j in range(len(a)):
             if(a[j] == i):
                 flag = 1
                 break
             
         if (flag==0):
             return i
+   
+    return -1
 
-    print(i)
+    
 
-missingNumber([1,2,4,5],5)
+print(missingNumber([1,2,4,5],5))
 
 def missingNum(a:List[int],n:int) -> int:
     sum = n*(n+1)//2
@@ -22,6 +25,6 @@ def missingNum(a:List[int],n:int) -> int:
         s += a[i]
 
     val = sum-s
-    print(val)
+    # print(val)
 
 missingNum([1,2,4,5],5)
