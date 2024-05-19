1201d200833b32cc63648437dcd260dc6e33d776
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-19 20:54:26 +0530

    sorting arrays with 0's ans 1's

--- 0s_1s_sort.py
+++ 0s_1s_sort.py
@@ -25,3 +25,38 @@ def sortColors(nums: List[int]) -> None:
 
 sortColors([2,0,2,1,1,0])
 
+
+
+
+#               0        low-1     low       mid-1         high+1     n-1
+#               ^         ^         ^         ^              ^         ^
+#               |         |         |         |              |         |
+#               0 0 0 0 0 0         1 1 1 1 1 1   unsorted   2 2 2 2 2 2
+#                                               ~~~~~~~~~~~~
+
+
+
+
+def sortColors2(arr: List[int]) -> None:
+    n = len(arr)
+    low = 0
+    mid = 0
+    high = n-1
+
+    arr[low] = 0
+    arr[mid] = 0
+    while (mid <= high):
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+
+        elif arr[mid] == 1:
+            mid += 1
+
+        elif arr[mid] == 2:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+    print(arr)
+
+sortColors2([2,0,2,1,1,0])
