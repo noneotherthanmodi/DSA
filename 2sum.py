f1344c6276ad22182854c49eeac9fb9efe6ffa2c
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-19 15:19:16 +0530

    finding target sum from given array

--- 2sum.py
+++ 2sum.py
@@ -1,4 +1,15 @@
 from typing import List
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    sum = 0
+def twoSum(nums: List[int], target: int) -> List[int]:
     n = len(nums)
+    for i in range(n):
+        rem = target - nums[i]
+        for j in range(i+1,n):
+            if rem==nums[j]:
+                
+                a = (i,j)
+                print(list(a))
+                return list(a)
+
+twoSum([3,2,4],6)
+
+
