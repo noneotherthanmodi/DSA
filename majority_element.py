984864384c5e5b43b4297a9b44995e91303ab6c8
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-20 00:35:45 +0530

    finding element occured more than n/2 times

--- majority_element.py
+++ majority_element.py
@@ -2,19 +2,31 @@ from typing import List
 from collections import Counter
 
 
+#MOST OPTIMAL ONE ->  MOORE'S VOTING METHOD
 
-#MORE OPTIMAL ONE ->
 def majorityElement1(nums: List[int]) -> int:
     n = len(nums)
+    count = 0
+    for i in range(n):
+        if count == 0:
+            count = 1
+            element = nums[i]
 
-    counter = Counter(nums)
-    
-    for num, count in counter.items():
-        if count > (n//2):
-            print(num)
+        elif nums[i] == element:
+            count += 1
+        
+        else:
+            count -= 1
 
-    return -1
 
+    count1 = 0
+    for i in range(n):
+        if (nums[i] == element):
+            count1 += 1
+
+    if count1 > (n // 2):
+        print(element)
+        return element
 
 
 majorityElement1([3,2,3])
@@ -28,8 +40,45 @@ majorityElement1([3,2,3])
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+#MORE OPTIMAL ONE ->
+def majorityElement2(nums: List[int]) -> int:
+    n = len(nums)
+
+    counter = Counter(nums)
+    
+    for num, count in counter.items():
+        if count > (n//2):
+            print(num)
+
+    return -1
+
+
+
+# majorityElement2([3,2,3])
+
+
+
+
+
+
+
+
+
+
 #NOT BETTER ONE -> 
-def majorityElement(nums: List[int]) -> int:
+def majorityElement3(nums: List[int]) -> int:
     n = len(nums)
     
     for i in range(len(nums)):
@@ -43,4 +92,4 @@ def majorityElement(nums: List[int]) -> int:
 
 
 
-# majorityElement([3,2,3])
+# majorityElement3([3,2,3])
