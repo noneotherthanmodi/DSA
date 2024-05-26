8bb545cd861844e74493a9015a22a45bb389eef2
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-27 03:33:57 +0530

    arranging elements by signs

--- arrnge_ele_by_sign.py
+++ arrnge_ele_by_sign.py
@@ -1,4 +1,6 @@
 from typing import List
+
+#this includes the positives and negatives of not same length -> 
 def alternateNumbers(a : List[int]) -> List[int]:
     # Write your code here.
     pos = []
@@ -11,15 +13,35 @@ def alternateNumbers(a : List[int]) -> List[int]:
         else:
             neg.append(a[i])
 
+    if((len(pos)) > (len(neg))):
+        for i in range(len(neg)):
+            a[2*i] = pos[i]
+            a[2*i + 1] = neg[i]
+        
+        index = len(neg) * 2
+        for i in range(len(neg), len(pos)):
+            a[index] = pos[i]
+            index += 1
+
+
+    else:
+        for i in range(len(pos)):
+            a[2*i] = pos[i]
+            a[2*i + 1] = neg[i]
+        
+        index = len(pos) * 2
+        for i in range(len(pos), len(neg)):
+            a[index] = neg[i]
+            index += 1 
     
-    for i in range(n//2):
-        a[2*i] = pos[i]
-        a[2*i + 1] = neg[i]
+    
 
         
     print(a)
 
-alternateNumbers([1, 2, -3, -1, -2, 3])
+alternateNumbers([1, 2, -3, -1, -2, 3, -4, -5, -6])
+
+
 
 
 
