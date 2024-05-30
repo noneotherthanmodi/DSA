9c4746b71656b2f12fc4c2017d35afac22f2146f
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-30 16:15:53 +0530

    pascals' triangle with variety

--- Array/pascal_triangle.py
+++ Array/pascal_triangle.py
@@ -8,12 +8,32 @@ def nCr(n:int, r: int):            #take r-1 and c-1 for pascal's triangle
     res = 1
     for i in range(r):
         res = res * (n-i)
-        res /= (i+1)
-    print(res)
+        res = res // (i+1)
+    return(res)
+
+
+def find_nth_ele(r,c):
+    element = nCr(r-1,c-1)
+    print (element)
+find_nth_ele(5,3)
+
+
+
+def print_entire_row(n):
+    for c in range(1,n+1):
+        print(nCr(n-1,c-1),end = "")
+
+print_entire_row(2)
 
 
 
 
 def generate(numRows: int) -> List[List[int]]:
-    for i in range(numRows):
-        pass 
+    ans = 1
+    if numRows == 1:
+        return 1
+    
+        
+
+
+# generate(4)
