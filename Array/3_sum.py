3ad70cf78b349a5b2a78cbe043ceb908f75bd045
noneotherthanmodi <uditmodi03@gmail.com>
2024-05-30 21:48:23 +0530

    finding unique triplets for required sum

--- Array/3_sum.py
+++ Array/3_sum.py
@@ -17,3 +17,31 @@ def threeSum(nums: List[int]) -> List[List[int]]:
     print(ans)
 
 threeSum([-1, 0, 1, 2, -1, -4])
+
+
+
+#better with T(): O(N^2 * log(no. of unique triplets)), where N = size of the array.
+def threeSum2(nums: List[int]) -> List[List[int]]:
+    n = len(nums)
+    
+    st = set()
+    for i in range(n):
+        hashnet = set()
+
+        for j in range(i+1,n):
+            third = -(nums[i] + nums[j])
+
+            if third in hashnet:
+                temp = [nums[i],nums[j],third]
+                temp.sort()
+                st.add(tuple(temp))
+
+            hashnet.add(nums[j])
+             
+                
+    ans = list(st)
+
+    print(ans)
+
+
+threeSum2([-1, 0, 1, 2, -1, -4])
